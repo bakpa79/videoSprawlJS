5e6138f35c35f44be73955ccc30c09f8e2f692ad
Chetan Shenoy <me@chetanshenoy.com>
2014-01-29 13:02:35 -0500

    added the example to its own folder. made some changes to videosprawl.

--- videoSprawl.js
+++ videoSprawl.js
@@ -6,62 +6,60 @@
  
     $.fn.videoSprawl = function() {
  
-        //function to scrape and grab all the data-attributes in to an objects
-        strJson = '{'
-        $.each($(this).data(), function(i, v) {
-            strJson += i + ":'" + v + "',";
-        });
-        strJson = strJson.substring(0, strJson.length - 1);
-        strJson += '}';
-        var jsonObject = eval('(' + strJson +')');
-
-
+        var jsonObject = $(this).data();
 
         //function to id and filter specific data elements. In this case, attibutes that need to be dropped into the <video> tag as tag options ie. autoplay, loop, etc
-        function show_props(obj, objName) {
-          var result = "";
-          var posterResult = "";
-          for (var prop in obj) {posterResulturl = obj[posterResult];
-            //}
-            if (prop == "autoplay") {
-            	autoplayEl = obj.autoplay;
-            	autoplayProp = prop;
-            	console.log(prop+" "+autoplayEl);
-            	delete obj.autoplay;
-            } else if(prop == "loop") {
-            	loopEl = obj.loop;
-            	loopProp = prop;
-            	console.log(loopProp+" "+loopEl);
-            	delete obj.loop;
-            } else if(prop == "muted") {
-            	mutedEl = obj.muted;
-            	mutedProp = prop;
-            	console.log(mutedProp+" "+mutedEl);
-            	delete obj.muted;
-            } else if(prop == "volumn") {
-            	volumnEl = obj.volumn;
-            	volumnProp = prop;
-            	console.log(volumnProp+" "+volumnEl);
-            	delete obj.volumn;
-            } else if(prop == "poster") {
-            	posterEl = obj.poster;
-            	posterProp = prop;
-            	console.log(prop+" "+posterEl);
-            	delete obj.poster;
-            };
-            //for anything that isn't an attribute above, drop in as src videos
-            result += "<source src="+obj[prop]+ " type=\"video/"+prop+ "\">\n";
-          }
-          //return the <video> tag
-          return "<video "+ volumnProp +"=\"" +volumnEl +"\""+ mutedProp+"=\"" +mutedEl +"\" "+ loopProp + "=\"" +loopEl +"\""+ autoplayProp +"=\"" + autoplayEl +"\""+ posterProp+"=\"" +posterEl +"\">" + result + "</video>";
+        function show_props(obj) {
+            var $video = $("<video />");
+            for (var prop in obj) {
+
+                switch (prop) {
+                    case "autoplay":
+                        $video.attr({
+                            autoplay: obj.autoplay
+                        });
+                        break;
+                    case "loop":
+                        $video.attr({
+                            loop: obj.loop
+                        });
+                        break;
+                    case "muted":
+                        $video.attr({
+                            muted: obj.muted
+                        });
+                        break;
+                    case "volume":
+                        $video.attr({
+                            volume: obj.volume
+                        });
+                        break;
+                    case "poster":
+                        $video.attr({
+                            poster: obj.poster
+                        });
+                        break;
+                    case "preload":
+                        if(obj.preload) $video.attr("preload" , "");
+                        break;
+                    case "controls":
+                        if(obj.controls) $video.attr("controls", "");
+                        break;
+
+                    default:
+                        $video.append("<source src="+obj[prop]+ " type=\"video/"+prop+ "\">");
+                }
+            }
+            //return the <video> tag
+            return $video;
         }
 
-        show_props(jsonObject, "jsonObject");
+        var video = show_props(jsonObject, "jsonObject");
 
-        if ($('html').hasClass('touch')) {
-        	// detect if the html element has touch class via Modernizr; if is mobile, ie, no autoplay, Do Nothing
+        if ($("html").hasClass("touch")) {
+            // detect if the html element has touch class via Modernizr; if is mobile, ie, no autoplay, Do Nothing
         } else {
-        	$(this).replaceWith(show_props(jsonObject, "jsonObject"));
+            $(this).replaceWith(video);
         }
  
     };
